7680a59b2dbaac007650cde45543ce0153144e33
eXory2024 <exory@anio.software>
2024-09-08 14:18:17 +0200

    Update and adapt to @4tune-poc/realm-js-and-web-base.

--- src/index.template.mjs
+++ src/index.template.mjs
@@ -3,21 +3,23 @@ import process from "node:process"
 
 import {
 	searchForConfigFile,
-	generateRuntimeData,
+	generateRuntimeInitData,
 	rollupPlugin
 } from "@4tune-poc/realm-js-and-web-base"
 
 import {
-	initializeRuntimeFromData
+	initializeRuntime
 } from "@4tune-poc/realm-js-and-web-base/runtime"
 
 const project_root = await searchForConfigFile(path.dirname(process.argv[1]))
 
-const runtime_data = await generateRuntimeData(
-	project_root,
-	await rollupPlugin(project_root)
-)
+const {ctx} = await rollupPlugin(project_root)
+const {runtime_init_data, project_resources} = ctx
 
-const runtime = await initializeRuntimeFromData(runtime_data)
+const runtime = await initializeRuntime(runtime_init_data, project_resources)
+
+export function loadResource(url) {
+	return runtime.loadResourceDynamic(url)
+}
 
 //$$$runtime_glue_code$$$//
